a598fa98bdec61e11995918df62ecd0c11c8163c
Pavel Lobashov <shockwavenn@gmail.com>
2019-08-27 18:10:27 +0300

    Add working test for export

--- test/test_zeppelin.py
+++ test/test_zeppelin.py
@@ -3,6 +3,26 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
+import tempfile
+import json
+import codecs
+
+
+class Browser:
+    download_dir = tempfile.mkdtemp()
+    chromedriver_path = r'/usr/bin/chromedriver'
+
+    def __init__(self):
+        chrome_options = webdriver.ChromeOptions()
+        prefs = {'download.default_directory': self.download_dir}
+        chrome_options.add_experimental_option('prefs', prefs)
+        self.browser = webdriver.Chrome(executable_path=self.chromedriver_path, chrome_options=chrome_options)
+
+    def open(self, url):
+        self.browser.get(url)
+
+    def quit(self):
+        self.browser.quit
 
 
 class MainPage:
@@ -11,20 +31,20 @@ class MainPage:
         self.wait_to_load()
 
     def wait_to_load(self):
-        WebDriverWait(self.driver, 3).until(
+        WebDriverWait(self.driver.browser, 3).until(
             EC.presence_of_element_located((By.XPATH, '//div[@id="main"]//a[@data-target="#noteCreateModal"]')))
 
     def open_new_note_window(self):
-        self.driver.find_element_by_xpath('//div[@id="main"]//a[@data-target="#noteCreateModal"]').click()
-        WebDriverWait(self.driver, 3).until(
+        self.driver.browser.find_element_by_xpath('//div[@id="main"]//a[@data-target="#noteCreateModal"]').click()
+        WebDriverWait(self.driver.browser, 3).until(
             EC.presence_of_element_located((By.XPATH, '//div[@id="noteCreateModal"]//input[@id="noteName"]')))
         time.sleep(1)  # Remove timeout
 
     def create_new_note(self, name, interpreter='spark'):
         self.open_new_note_window()
-        self.driver.find_element_by_xpath('//div[@id="noteCreateModal"]//input[@id="noteName"]').send_keys(name)
+        self.driver.browser.find_element_by_xpath('//div[@id="noteCreateModal"]//input[@id="noteName"]').send_keys(name)
         # TODO: ability to set interpreter
-        self.driver.find_element_by_xpath('//*[@id="createNoteButton"]').click()
+        self.driver.browser.find_element_by_xpath('//*[@id="createNoteButton"]').click()
         return Notebook(self.driver)
 
 
@@ -34,26 +54,28 @@ class Notebook:
         self.wait_to_load()
 
     def wait_to_load(self):
-        WebDriverWait(self.driver, 3).until(
+        WebDriverWait(self.driver.browser, 3).until(
             EC.presence_of_element_located((By.XPATH, '//textarea[contains(@class, "ace_text-input")]')))
 
     def type_note(self, note):
-        self.driver.find_element_by_xpath('//textarea[contains(@class, "ace_text-input")]').send_keys(note)
+        self.driver.browser.find_element_by_xpath('//textarea[contains(@class, "ace_text-input")]').send_keys(note)
+
+    def name(self):
+        return self.driver.browser.find_element_by_xpath('//p[contains(@class, "form-control-title")]/span').text
 
     def export_note(self):
-        self.driver.find_element_by_xpath('//button[contains(@ng-click, "exportNote")]').click()
-
-
-def start_browser():
-    chromedriver_path = r'/usr/bin/chromedriver'
-    return webdriver.Chrome(executable_path=chromedriver_path)
+        self.driver.browser.find_element_by_xpath('//button[contains(@ng-click, "exportNote")]').click()
+        file_path =self.driver.download_dir + "/" + self.name() + ".json"
+        return json.load(codecs.open(file_path, 'r', 'utf-8-sig'))
 
 
 def test_export():
-    driver = start_browser()
-    driver.get('http://localhost:8080')
+    driver = Browser()
+    driver.open('http://localhost:8080')
     main_page = MainPage(driver)
     note_page = main_page.create_new_note('note_name')
     note_page.type_note('note_content')
-    note_page.export_note()
+    note_data = note_page.export_note()
+    assert note_data['name'] == 'note_name'
+    assert note_data['paragraphs'][0]['text'] == 'note_content'
     driver.quit()
